ea111be8f84ef4393886a1d8ea2356fc2aa38595
Casey Wilson <CWilson@Sarahs-MacBook-Air.local>
2021-02-05 16:08:13 -0600

    redesign scheme

---
+++ holder.js
@@ -0,0 +1,42 @@
+  <div class="skills-inner-container">
+                <div class="skills-card">
+                    <h2>Languages</h2>
+                    <!-- <ul> -->
+                        <!-- <li>HTML</li>
+                        <li>CSS</li>
+                        <li>JavaScript</li> -->
+                        <p>I know HTML, CSS, and JavaScript</p>
+                    <!-- </ul> -->
+                </div>
+                <div class="skills-card">
+                    <h2>Frontend</h2>
+                    <ul>
+                        <li>React</li>
+                        <li>Redux</li>
+                        <li>Material UI</li>
+                        <li>Materlialize CSS</li>
+                        <li>Boostrap</li>
+                    </ul>
+                </div>
+                <div class="skills-card">
+                    <h2>Backend</h2>
+                    <ul>
+                        <li>Node</li>
+                        <li>Express</li>
+                        <li>MongoDB</li>
+                        <li>PostgreSQL</li>
+                    </ul>
+
+
+                </div>
+                <div class="skills-card">
+                    <h2>Other</h2>
+                    <ul>
+                        <li>Git</li>
+                        <li>JSON</li>
+                        <li>APIs</li>
+                        <li>REST</li>
+                    </ul>
+
+                </div>
+            </div>
